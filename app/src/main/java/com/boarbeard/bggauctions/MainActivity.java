2f9ac3b5c7530ec98dbe79e407d5e72d499f5a32
Leif Norcott <nibuen@gmail.com>
2016-11-16 11:36:56 -0800

    Adding VCS and encoding info

--- app/src/main/java/com/boarbeard/bggauctions/MainActivity.java
+++ app/src/main/java/com/boarbeard/bggauctions/MainActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.boarbeard.bggauctions.io.Adapter;
+import com.boarbeard.bggauctions.model.GeekListEntry;
 import com.boarbeard.bggauctions.model.GeekListsResponse;
 
+import java.util.List;
+
 import io.reactivex.Observable;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +26,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Timber.v("Trying out a request.");
         Observable<GeekListsResponse> observable = Adapter.createForXml().geekLists();
-        observable.subscribeOn(Schedulers.io())
-                .map(response -> response.getGeekListEntries());
+
+        Single<List<GeekListEntry>> listSingle = observable.subscribeOn(Schedulers.io())
+                .map(new Function<GeekListsResponse, List<GeekListEntry>>() {
+                    @Override
+                    public List<GeekListEntry> apply(GeekListsResponse geekListsResponse) throws Exception {
+                        return geekListsResponse.getGeekListEntries();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .firstOrError();
+
+        Timber.v(listSingle.toString());
     }
 
 
